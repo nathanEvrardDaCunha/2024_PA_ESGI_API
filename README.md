a7ff1146b60b9f6fc06d23331b447d2826cd2cba
Zharks <nevraddacunha@myges.fr>
2024-04-01 13:09:52 +0200

    style: clean up lot of local file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 2024_PA_ESGI
 # Dev v0.0.1
+
+# Instruction v1 :
+- Run : npm install
+- Run : prisma generate
+- Modify the .env
+- Run : docker-compose build
+- Run : docker-compose up
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import {PrismaClient} from "@prisma/client";
 import {initRoutes} from "./handlers/routes";
 import express from "express";
 
