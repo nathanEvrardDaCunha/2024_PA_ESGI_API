29a0b2fdbe4a552b13981f9dd298f707bf56e938
Zharks <nevraddacunha@myges.fr>
2024-04-06 23:52:19 +0200

    feat: post route for bill ; deleted bill relation to Location in DB

--- api/src/repository/billRepository.ts
+++ api/src/repository/billRepository.ts
@@ -1,10 +1,20 @@
-import {prisma} from "../index";
+import { prisma } from "../index";
+import {Prisma} from "@prisma/client";
 
 export async function getAllBill() {
 	try {
 		return await prisma.bill.findMany();
 	} catch (error) {
-		console.error('Error fetching persons:', error);
+		console.error('Error fetching bills:', error);
+		throw error;
+	}
+}
+
+export async function createBill(data: Prisma.BillCreateInput) {
+	try {
+		return await prisma.bill.create({data});
+	} catch (error) {
+		console.error('Error creating bills:', error);
 		throw error;
 	}
 }
