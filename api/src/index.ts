e8213c3172ae4f7dac0bf39db70fc2387b42d50f
Zharks <nevraddacunha@myges.fr>
2024-04-19 19:00:34 +0200

    feat: adding corse and enable for request

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,23 @@
 import {PrismaClient} from "@prisma/client";
 import {initRoutes} from "./handlers/routes";
 import express from "express";
+import cors from "cors"; // Import the cors middleware
 
 export const prisma = new PrismaClient();
 
 async function main() {
     const app = express();
     const port = 3000;
+    
+    // Enable CORS for all routes
+    app.use(cors({
+        origin: "http://localhost:3001", // Replace with the URL of your React client
+        optionsSuccessStatus: 200, // Some legacy browsers require this option to be set
+    }));
+    
     app.use(express.json());
     initRoutes(app);
-
+    
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
     });
