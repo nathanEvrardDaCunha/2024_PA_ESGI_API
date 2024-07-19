a790191219baa7d80017b0aac362f73467f221ef
Astralal <benzekkitalal@gmail.com>
2024-07-19 16:56:24 +0200

    feat: modif des redirection

--- api/dist/handlers/membershipRoutes.js
+++ api/dist/handlers/membershipRoutes.js
@@ -102,8 +102,8 @@ membershipRouter.post('/create-checkout-session', (req, res) => __awaiter(void 0
                 },
             ],
             mode: 'payment',
-            success_url: 'http://localhost:3001/membership-success?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url: 'http://localhost:3001/membership',
+            success_url: 'http://185.216.27.210:3001/membership-success?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url: 'http://185.216.27.210:3001/membership',
             metadata: {
                 userId,
                 type,
